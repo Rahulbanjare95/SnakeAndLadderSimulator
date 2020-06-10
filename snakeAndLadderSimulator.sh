865c2d13307d5c1b38e9f311a6a4abc37d7d28e7
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-11 00:26:19 +0530

    Added player Plays till 100 achieved

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,36 +1,49 @@
-#!/bin/bash -x
+#!/bin/bash
 	echo "Welcome to Snake and Ladder Simulator"
 
 	PLAYER_NO=1
 	START_POSITION=0
-	#below variables defines the movement for player
 	NO_PLAY_CASE=0
 	LADDER_CASE=1
 	SNAKE_CASE=2
+	WIN_POSITION=100
 
-	#function generates a dice role by generating a random number 1 to 6
 	function DiceRoller(){
 		roller=$((RANDOM%6+1))
-		return $roller
 	}
 
 	currentPosition=$START_POSITION
 
 	function gamePlaySimulation(){
-	optionGenerator=$((RANDOM%3))
 
+	optionGenerator=$((RANDOM%3))
 	case $optionGenerator in
 	$NO_PLAY_CASE)
-		currentPosition=$((currentPosition+0))
+		currentPosition=$(( currentPosition + 0 ))
+		echo "Face Value = 0 NO_PLAY  position = $currentPosition"
 		;;
    $LADDER_CASE)
 		DiceRoller
-		currentPosition=$((currentPosition + roller))
+		currentPosition=$(( currentPosition + roller ))
+		echo "Face Value = $roller LADDER  position = $currentPosition"
 		;;
    $SNAKE_CASE)
 		DiceRoller
-		currentPosition=$((currentPosition - roller))
+			currentPosition=$(( currentPosition - roller ))
+		echo "Face Value = $roller SNAKE position = $currentPosition"
 		;;
 	esac
 	}
+
+	while [ true ]
+	do
 	gamePlaySimulation
+		if [ $currentPosition -eq $WIN_POSITION ]
+		then
+			break
+		elif [ $currentPosition -lt $START_POSITION ]
+		then
+			currentPosition=$START_POSITION
+		fi
+	done
+	echo "Current Position = $currentPosition  so Reached Final"
