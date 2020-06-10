e8ce4128d97067cf9483792bf14830a1b8f4b1ae
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-10 07:22:05 +0530

    Added A fucntion to generate Dice Roll

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -3,4 +3,9 @@
 
 	PLAYER_NO=1
 	START_POSITION=0
-
+	#function generates a dice role by generating a random number 1 to 6
+	function DiceRoller(){
+		roller=$((RANDOM%6+1))
+		return $roller
+	}
+	DiceRoller
