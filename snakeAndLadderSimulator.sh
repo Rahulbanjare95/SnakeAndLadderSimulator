3f751f3e7ce5f8a9f845dca5a79ce6cf168dfc63
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-10 08:01:38 +0530

    Added GamePLaySimulation function to switch positions of a player

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -3,9 +3,34 @@
 
 	PLAYER_NO=1
 	START_POSITION=0
+	#below variables defines the movement for player
+	NO_PLAY_CASE=0
+	LADDER_CASE=1
+	SNAKE_CASE=2
+
 	#function generates a dice role by generating a random number 1 to 6
 	function DiceRoller(){
 		roller=$((RANDOM%6+1))
 		return $roller
 	}
-	DiceRoller
+
+	currentPosition=$START_POSITION
+
+	function gamePlaySimulation(){
+	optionGenerator=$((RANDOM%3))
+
+	case $optionGenerator in
+	$NO_PLAY_CASE)
+		currentPosition=$((currentPosition+0))
+		;;
+   $LADDER_CASE)
+		DiceRoller
+		currentPosition=$((currentPosition + roller))
+		;;
+   $SNAKE_CASE)
+		DiceRoller
+		currentPosition=$((currentPosition - roller))
+		;;
+	esac
+	}
+	gamePlaySimulation
